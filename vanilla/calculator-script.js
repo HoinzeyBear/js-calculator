9c14d3c68707d2820cd671bc5fcbb0e8b7b0b631
HoinzeyBear <darrenhoyne@gmail.com>
2021-03-13 14:10:23 +0000

    Did some refactoring and added a result view and allowed you to chain from one result straight into the next calculation without having to clear. Going to commit this, find a calculator online and copy that into this repo and learn what I can from the differences. To note, I did not put huge effort into making a great calculator this is a learning exercise

--- vanilla/calculator-script.js
+++ vanilla/calculator-script.js
@@ -1,4 +1,5 @@
 var gridItems = document.getElementsByClassName("grid-item")
+var resultView = document.getElementById("resultview")
 var textView = document.getElementById("textview")
 var inputString=''
 var lastSelected=[]
@@ -14,37 +15,69 @@ for(var i =0; i< gridItems.length; i++) {
         return function(){
             inputString = document.getElementById(id).textContent
             if(isNaN(n)){
-                if(id === 'clear') {
-                    textView.textContent = ''
-                    firstNumber=''
-                    secondNumber=''
-                    lastSelected=[]
-                    return
-                }
-                if(firstNumber.length === 0) {
-                    firstNumber = Number.parseInt(lastSelected.join(''))
-                } else if(secondNumber.length === 0) {
-                    secondNumber = Number.parseInt(lastSelected.join(''))
-                }
-                if(id === 'equals') {
-                    inputString = '=' + operation(firstNumber, secondNumber)
-                    firstNumber = ''
-                    secondNumber = ''
-                } else {
-                    operation = calculator[id]
-                }
-                lastSelected = []
+                nonNumberSelected(n, id)
             } else {
-                lastSelected.push(n)
+                numberSelected(n)
             }
-            textView.textContent = textView.textContent + inputString
         }
     }
 
     gridItem.addEventListener("click", onClick(num, gridItem.id))
 }
 
+var nonNumberSelected = function(input, id) {
+    if(id === 'clear') {
+        clearSelected()
+        return
+    }
 
+    if(firstNumber.length === 0) {
+        firstNumber = Number.parseInt(lastSelected.join(''))
+    } else if(secondNumber.length === 0 && lastSelected.length > 0 ) {
+        secondNumber = Number.parseInt(lastSelected.join(''))
+    }   
+    
+    if(id === 'equals') {
+        equalsSelected()
+    } else {
+        operation = calculator[id]
+        appendTextviewContent(input)
+    }
+    lastSelected = []
+}
+
+var clearSelected = function() {
+    textView.textContent = ''
+    resultView.textContent = ''
+    firstNumber=''
+    secondNumber=''
+    lastSelected=[]
+}
+
+var equalsSelected = function() {
+    var value = operation(firstNumber, secondNumber)
+    firstNumber = value
+    secondNumber = ''
+    updateTextviewContent(value)
+    updateResultViewContent(value)
+}
+
+var numberSelected = function(input) {
+    lastSelected.push(input)
+    appendTextviewContent(input)
+}
+
+var updateTextviewContent = function(value) {
+    textView.textContent = value
+}
+
+var appendTextviewContent = function(value) {
+    textView.textContent = textView.textContent + value
+}
+
+var updateResultViewContent = function(value) {
+    resultView.textContent = value
+}
 
 var calculator = (function(){
     return {
